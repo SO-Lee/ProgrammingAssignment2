5c0df2942fc8190223e4e9df01c944de9dda69ab
SO-Lee <sol_library@hotmail.com>
2015-02-21 17:58:37 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function set up to read variable x as a matrix
+## establishes 4 sub-functions as set, get, setinverse and getinverse
+## set and get sets up variables x, y and m
+## setinverse reads the value of inverse determined in cacheSolve
+## getinverse returns the value of m
+## getinverse returns the value of m
+## mat.list generates the functions set, get, setinverse and getinverse as hash values and publishes that as a global environmental variable
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  mat.list<<-list(set = set, get = get,
+                  setinverse = setinverse,
+                  getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve first checks for a value in m, if not null, returns the value of m
+## if null, data variable reads value of the get function as per mat.list
+## solve is the function applied to data variable to return the inverse of the matrix read into x, and returns the value into m, which is then printed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- mat.list$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- mat.list$get()
+  m <- solve(data, ...)
+  mat.list$setinverse(m)
+  m
 }
